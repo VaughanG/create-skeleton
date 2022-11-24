bb104fbda465d065868afcce6b2f7a9f3cde8b89
VaughanG <david@davidg.org>
2022-11-24 18:42:53 +1300

    ok

---
+++ csapp/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	import { Divider } from '@brainandbones/skeleton'
+</script>
+
+<div>
+<Divider borderWidth="border-t-8" borderStyle="border-solid" borderColor="border-primary-500"/>
+</div>
