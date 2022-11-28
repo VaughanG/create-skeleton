3011b21ab4fe546f5b54511d5bef88a96f5f87d9
VaughanG <david@davidg.org>
2022-11-29 05:05:25 +1300

    Tooltip probs

--- csapp/src/routes/+page.svelte
+++ csapp/src/routes/+page.svelte
@@ -1,8 +1,17 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
 <script>
-	import { Divider } from '@brainandbones/skeleton'
+	import { Divider } from '@brainandbones/skeleton';
+	
+	import { Tooltip, Button } from 'flowbite-svelte';
+	//import { tooltip } from '@brainandbones/skeleton';
 </script>
 
 <div>
 <Divider borderWidth="border-t-8" borderStyle="border-solid" borderColor="border-primary-400"/>
 </div>
+<br>
+
+
+<Button id="hover">Tooltip hover</Button>
+<Tooltip triggeredBy="#hover">Hover tooltip content</Tooltip>
+<!-- <button use:tooltip={{ content: 'Skeleton' }}>Trigger</button> -->
--- csapp/tailwind.config.cjs
+++ csapp/tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
 		require('path').join(require.resolve('@brainandbones/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
@@ -11,6 +12,7 @@ module.exports = {
 	plugins: [
 		require('@tailwindcss/forms'),
 		require('@tailwindcss/typography'),
-		require('@brainandbones/skeleton/tailwind/theme.cjs')
+		require('@brainandbones/skeleton/tailwind/theme.cjs'),
+		require('flowbite/plugin')
 	]
 };
